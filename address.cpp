6ec9ca02e1e804a976e32f99a39c993d1db20628
John Ankcorn <jca@alum.mit.edu>
2015-12-01 13:13:49 -0500

    added comment

--- address.cpp
+++ address.cpp
@@ -80,7 +80,7 @@ extern "C" void *llvm_translate_malloc(size_t size, const Type *type, const Stru
     return ptr;
 }
 
-static void recursiveDelete(Value *V)
+static void recursiveDelete(Value *V) //nee: RecursivelyDeleteTriviallyDeadInstructions
 {
     Instruction *I = dyn_cast<Instruction>(V);
     if (!I)
