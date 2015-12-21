fc0c09b9029d57958276abc6e1b30f5499e9da31
John Ankcorn <jca@alum.mit.edu>
2015-12-21 10:25:34 -0500

    new generated files

---
+++ generated/l_class_OC_EchoRequest.v
@@ -0,0 +1,9 @@
+module l_class_OC_EchoRequest (
+    input CLK,
+    input nRST);
+    always @( posedge CLK) begin
+      if (!nRST) begin
+      end // nRST
+    end // always @ (posedge CLK)
+endmodule 
+
