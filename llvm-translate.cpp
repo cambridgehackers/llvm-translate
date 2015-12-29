acd48d2a22b38c9f3c5b528be3e82f33df3ae2f0
John Ankcorn <jca@alum.mit.edu>
2015-12-29 11:07:22 -0500

    cleanup

--- llvm-translate.cpp
+++ llvm-translate.cpp
@@ -41,7 +41,6 @@ cl::opt<std::string> OutputDir("odir", cl::init(""), cl::desc("<output directory
 int trace_full;// = 1;
 static int dump_interpret;// = 1;
 ExecutionEngine *EE;
-static const Function *EntryFn;
 
 /*
  * Read/load llvm input files
@@ -110,14 +109,14 @@ printf("[%s:%d] start\n", __FUNCTION__, __LINE__);
 printf("[%s:%d] now run main program\n", __FUNCTION__, __LINE__);
     DebugFlag = dump_interpret != 0;
     // Run main
-    std::vector<std::string> InputArgv;
-    InputArgv.push_back("param1");
-    InputArgv.push_back("param2");
-    EntryFn = Mod->getFunction("main");
-    if (!EntryFn) {
-        printf("'main' function not found in module.\n");
-        exit(1);
-    }
+    //std::vector<std::string> InputArgv;
+    //InputArgv.push_back("param1");
+    //InputArgv.push_back("param2");
+    //const Function *EntryFn = Mod->getFunction("main");
+    //if (!EntryFn) {
+        //printf("'main' function not found in module.\n");
+        //exit(1);
+    //}
     //char *envp[] = {NULL};
     //int Result = EE->runFunctionAsMain(EntryFn, InputArgv, envp);
 //printf("[%s:%d] %d\n", __FUNCTION__, __LINE__, Result);
