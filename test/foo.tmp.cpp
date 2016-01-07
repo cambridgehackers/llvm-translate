31cfb794b6789cbb4fd00de9889075eae200c2a0
John Ankcorn <jca@alum.mit.edu>
2016-01-07 15:24:59 -0500

    simplify test programs

--- test/foo.tmp.cpp
+++ test/foo.tmp.cpp
@@ -2,29 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-unsigned char *llvm_translate_malloc(unsigned long long size)
-{
-    return (unsigned char *)malloc(size);
-}
-unsigned char *memset(unsigned char *b, int c, unsigned long long len)
-{
-    return (unsigned char *)memset((void *)b, c, len);
-}
-// operator delete(void *)
-void _ZdlPv(unsigned char*)
-{
-}
-void __cxa_pure_virtual(void)
-{
-printf("[%s:%d]\n", __FUNCTION__, __LINE__);
-}
-
-unsigned int stop_main_program;
 #include "l_class_OC_Echo.h"
 #include "l_class_OC_Echo.cpp"
 #include "l_class_OC_Fifo1.cpp"
 #include "l_class_OC_FifoPong.cpp"
+
+unsigned int stop_main_program;
+class l_class_OC_EchoIndication zEchoIndication;
+class l_class_OC_Echo zEcho;
 
 bool l_class_OC_EchoIndication::heard__RDY(void) {
         return true;
@@ -32,27 +17,6 @@ bool l_class_OC_EchoIndication::heard__RDY(void) {
 void l_class_OC_EchoIndication::heard(unsigned int v) {
         printf((("Heard an echo: %d\n")), v);
         stop_main_program = 1;
-}
-
-class l_class_OC_EchoIndication zEchoIndication;
-class l_class_OC_Echo zEcho;
-
-void memdump(unsigned char *p, int len, const char *title)
-{
-int i;
-
-    i = 0;
-    while (len > 0) {
-        if (!(i & 0xf)) {
-            if (i > 0)
-                printf("\n");
-            printf("%s: ",title);
-        }
-        printf("%02x ", *p++);
-        i++;
-        len--;
-    }
-    printf("\n");
 }
 
 int main(int argc, const char *argv[])
